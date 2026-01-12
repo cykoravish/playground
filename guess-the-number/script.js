12568d2696aeede5cdc739875ecf1b375edff023
cykoravish <ravishbisht85@gmail.com>
2026-01-12 20:32:16 +0530

    declaring computer guess no.

--- guess-the-number/script.js
+++ guess-the-number/script.js
@@ -1,11 +1,14 @@
 const input = document.getElementById("inp");
 const submitBtn = document.getElementById("submit");
 
+let computerGuess = Math.floor(Math.random * 100);
+
 submitBtn.addEventListener("click", ()=>{
     let value = input.value;
     if(isNaN(value)){
         console.log("provided value is not a number - ",value)
+        input.value = "";
         return;
     }
-    console.log("all ok")
+    input.value = "";
 })
