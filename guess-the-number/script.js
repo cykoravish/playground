4020c7913e9b3239919886b445a462685017eb7a
cykoravish <ravishbisht85@gmail.com>
2026-01-12 20:28:40 +0530

    adding checks

--- guess-the-number/script.js
+++ guess-the-number/script.js
@@ -3,5 +3,9 @@ const submitBtn = document.getElementById("submit");
 
 submitBtn.addEventListener("click", ()=>{
     let value = input.value;
-    console.log(value)
+    if(isNaN(value)){
+        console.log("provided value is not a number - ",value)
+        return;
+    }
+    console.log("all ok")
 })
